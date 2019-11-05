b6f18af50b618244710f7b6311bebdfcb18a1a43
kuei0221 <michaelhwang0619@gmail>
2019-11-05 15:42:35 +0800

    Bug Fix: modifiy missions' factory. Now created_at is generate with sequence using DateTime.new rather than Faker::Date

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :mission do
     name {Faker::Lorem.sentence(word_count: 5)}
     content {Faker::Lorem.sentence(word_count: 20)}
-    sequence(:created_at){|d| Faker::Date.backward(days: d)}
+    sequence(:created_at){|d| DateTime.new(2019, 11, d)}
   end
 end
